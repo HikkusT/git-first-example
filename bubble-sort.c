4c9b81b82726667b92d5622eda68a04cef4f0845
Lucas <lucas@thatis.me>
2019-03-28 15:40:00 -0300

    Implement bubble sort
    Great way to sort numbers! Just iterate through the numbers and put them
    in increasing order.

--- bubble-sort.c
+++ bubble-sort.c
@@ -11,7 +11,13 @@ void swap(int *xp, int *yp)
 // A function to implement bubble sort
 void bubbleSort(int arr[], int n)
 {
-	// TODO
+int i, j;
+for (i = 0; i < n-1; i++)
+
+	// Last i elements are already in place
+	for (j = 0; j < n-i-2; j++)
+		if (arr[j] > arr[j+1])
+			swap(&arr[j], &arr[j+1]);
 }
 
 /* Function to print an array */
